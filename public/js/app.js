6c2dac8dfe43125f73ef39f734903b51f8276b40
onecode369 <onecode369@yandex.com>
2020-03-17 20:31:28 +0530

    Changed some code for deployment
    Signed-off-by: onecode369 <onecode369@yandex.com>

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ form.addEventListener("submit", e => {
     precipitationChanceOutput.textContent = "";
     forecastOutput.textContent = "";
     const location = locationInput.value;
-    fetch("http://localhost:3000/weather?address=" + location).then(res => {
+    fetch("/weather?address=" + location).then(res => {
       res.json().then(data => {
         document.getElementById("loader").style.display = "none";
         if (data.error) {
